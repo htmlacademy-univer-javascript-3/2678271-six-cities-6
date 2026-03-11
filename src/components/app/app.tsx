ef79aab09114b2afc083f6cd98f60227e9ad6bea
Yana Koryakina <y.koryakina@g.nsu.ru>
2026-03-12 02:12:31 +0700

    задание 3.9

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,11 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import { AppRoute, AuthorizationStatus } from '../../const';
 import Main from '../../pages/main/main';
+import Login from '../../pages/login/login';
+import Favorite from '../../pages/favorites/favorites';
+import Offer from '../../pages/offer/offer';
+import NotFound from '../../pages/not-found/not-found';
+import PrivateRoute from '../../pages/private-route/private-route';
 
 type MainProps = {
   rentalOffersNumber: number;
@@ -6,7 +13,35 @@ type MainProps = {
 
 function App({rentalOffersNumber}: MainProps) {
   return (
-    <Main rentalOffersNumber={rentalOffersNumber} />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<Main rentalOffersNumber={rentalOffersNumber} />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={
+            <PrivateRoute
+              authorizationStatus={AuthorizationStatus.NoAuth}
+            >
+              <Favorite />
+            </PrivateRoute>}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<Login />}
+        />
+        <Route
+          path={AppRoute.Offer}
+          element={<Offer />}
+        />
+        <Route
+          path="*"
+          element={<NotFound />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
