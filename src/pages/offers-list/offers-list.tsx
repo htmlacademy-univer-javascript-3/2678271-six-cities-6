68631d8cf40bfcf2f2ecdf47e6723077eb132d55
Yana Koryakina <y.koryakina@g.nsu.ru>
2026-03-17 17:57:37 +0700

    fix linter error

--- src/pages/offers-list/offers-list.tsx
+++ src/pages/offers-list/offers-list.tsx
@@ -10,7 +10,7 @@ type OfferListProps = {
 function OfferList(props: OfferListProps){
   const {offers} = props;
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  // eslint-disable-next-line
   const [activeCardId, setActiveCardId] = useState<string | null>(null);
 
   return(
